1e6f8ca49bb1237a76e99c103f8d062090d1c9ee
Dj3n <fernandezgarcia.jenny@gmail.com>
2025-02-25 11:34:55 +0100

    finalisation demo routing

--- demoAngular/src/app/app.module.ts
+++ demoAngular/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DemoRoutingModule } from './demo/demo-routing.module';
 import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
@@ -15,7 +14,7 @@ import { HomeComponent } from './components/home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DemoRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
