d4835918c7bb6a314142c1f28c991c185dd1a307
Dj3n <fernandezgarcia.jenny@gmail.com>
2025-04-08 11:33:20 +0200

    correction exe event

--- demoAngular/src/app/exercice/components/exe-event/exe-event.component.ts
+++ demoAngular/src/app/exercice/components/exe-event/exe-event.component.ts
@@ -1,3 +1,4 @@
+import { formatDate } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -7,20 +8,30 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './exe-event.component.html',
   styleUrl: './exe-event.component.css'
 })
+
 export class ExeEventComponent {
-    public eventForm? : FormGroup
+    public eventForm : FormGroup
 
     constructor(private _formBuilder : FormBuilder){
       this.eventForm = _formBuilder.group({
-        title : [null, 
-          [Validators.required],
-          [Validators.minLength(2)],
-          [Validators.maxLength(16)]
-        ],
+        title : [null,[ 
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(16)
+        ]],
         description: [null,[
-          
-        ]]
+          Validators.maxLength(512)
+        ]],
+        date: [formatDate(new Date(),'yyyy-MM-dd','en'),[Validators.required]],
+        allDay: [true,[]],
+        startTime : [formatDate(new Date(), 'hh:mm','en'),[]],
+        endTime : [formatDate(new Date().setHours(new Date().getHours()+1), 'hh:mm','en'),[]]
       })
+    }
+
+    public onSubmit():void {
+      if(!this.eventForm.valid) throw new Error('Formulaire invalide')
+        console.log(this.eventForm.value);
     }
 
 
